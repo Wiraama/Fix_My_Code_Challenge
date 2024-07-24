3b5b5dfec90c08c2a848a9cb6b11a7c0f2275f2a
mawira <mawira@students.uonbi.ac.ke>
2024-07-24 06:11:23 +0300

    square.py,

---
+++ 0x01-challenge/status_server/api/v1/square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+class Square():
+        
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+    def area_of_my_square(self):
+        """ Area of the square """
+        return self.width * self.height
+
+    def PermiterOfMySquare(self):
+        return (self.width * 2) + (self.height * 2)
+
+    def __str__(self):
+        return "{}/{}".format(self.width, self.height)
+
+if __name__ == "__main__":
+
+    s = Square(width=12, height=9)
+    print(s)
+    print(s.area_of_my_square())
+    print(s.PermiterOfMySquare())
